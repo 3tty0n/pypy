e4c50a13ac772a009bd5217a50090d058f14a3e3
YUSUKE IZAWA <me@yizawa.com>
2026-09-19 09:18:10 +0900

    Restore the three deferred-execution entries lost from the module definition

--- pypy/module/_metatensor/moduledef.py
+++ pypy/module/_metatensor/moduledef.py
@@ -11,6 +11,9 @@ class Module(MixedModule):
         'kernel_count': 'interp_tensor.kernel_count',
         'kernel_compile_count': 'interp_tensor.kernel_compile_count',
         'launch_count': 'interp_tensor.launch_count',
+        'lazy_enabled': 'interp_tensor.lazy_enabled',
+        'lazy_stats': 'interp_tensor.lazy_stats',
+        'mark_step': 'interp_tensor.mark_step',
         'mem_total': 'interp_tensor.mem_total',
         'alloc_failed': 'interp_tensor.alloc_failed',
     }
